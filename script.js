240bcb3194abd5778849586c1026f45569918b05
Robeil Aregawi <65261399+Robeil@users.noreply.github.com>
2020-07-14 22:14:25 -0700

    creating basic

---
+++ script.js
@@ -0,0 +1,7 @@
+let currentDay = document.getElementById("currentDay");
+let timeBlock = document.getElementById("timeBlock");
+let noteContainer = document.getElementById("noteContainer");
+let saveBtn = document.getElementById("save");
+let myInput = document.getElementById("myInput");
+let noteList = document.getElementById("note-list");
+
